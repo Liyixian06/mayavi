b63e5201148b73574150ecfdde9cacf05a792d2d
Liyixian06 <2575597322@qq.com>
2026-04-01 20:29:02 -0400

    Comment out unsafe code in sonar_fail_sample.py
    Comment out potentially dangerous functions and code.

--- sonar_fail_sample.py
+++ sonar_fail_sample.py
@@ -1,30 +1,30 @@
-import subprocess
+# import subprocess
 
-API_TOKEN = "ghp_1234567789_hardcoded_token"
+# API_TOKEN = "ghp_1234567789_hardcoded_token"
 
-def run_user_command(user_input):
-    subprocess.call("echo " + user_input, shell=True)
+# def run_user_command(user_input):
+#     subprocess.call("echo " + user_input, shell=True)
 
-def dangerous_eval(user_input):
-    return eval(user_input)
+# def dangerous_eval(user_input):
+#     return eval(user_input)
 
-def crash_with_none():
-    data = None
-    return len(data)
+# def crash_with_none():
+#     data = None
+#     return len(data)
 
-def divide_by_zero():
-    return 1 / 0
+# def divide_by_zero():
+#     return 1 / 0
 
-def swallow_all_errors():
-    try:
-        return dangerous_eval("2 + 2")
-    except:
-        return None
+# def swallow_all_errors():
+#     try:
+#         return dangerous_eval("2 + 2")
+#     except:
+#         return None
 
-def dangerous_exec(user_input):
-    exec(user_input)
+# def dangerous_exec(user_input):
+#     exec(user_input)
 
-if __name__ == "__main__":
-    print(run_user_command("ls -la"))
-    print(crash_with_none())
-    print(divide_by_zero())
+# if __name__ == "__main__":
+#     print(run_user_command("ls -la"))
+#     print(crash_with_none())
+#     print(divide_by_zero())
